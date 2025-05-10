0a6eedb134d21e45f3a59cc11610b37d3798679a
Veronica <no.demchenko@yandex.ru>
2025-05-10 11:21:54 +0300

    feat: задача переделать на O(N)

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// найти индекс самого большого элемента в масиве
+int greatestNumber(float[] array)
+{
+    int foundAtIndex = 0;
+    float foundMaxElement = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > foundMaxElement)
+        {
+            foundAtIndex = i;
+            foundMaxElement = array[i];
+        }
+    }
+    return foundAtIndex;
+}
+
+void main()
+{
+    float[] array = new float[] { 0, 5, 9, 1000 };
+    Console.WriteLine(
+        "Самое большое число в массиве находится под индексом = " + greatestNumber(array)
+    );
+}
+
+main();
