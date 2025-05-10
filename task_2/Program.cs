f714efad651d373b4e54f39f69b7aa0b0b6d5449
Veronica <no.demchenko@yandex.ru>
2025-05-10 21:22:51 +0300

    feat: 2 задание

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿Dictionary<string, int> makeCounterDictionary(string[] strArray)
+{
+    Dictionary<string, int> map = new Dictionary<string, int>();
+    foreach (var item in strArray)
+    {
+        if (map.ContainsKey(item))
+        {
+            map[item] = map[item] + 1;
+        }
+        else
+        {
+            map.Add(item, 1);
+        }
+    }
+    return map;
+}
+
+// вернет первое повторяющееся значение
+string getFirstDuplicatedElem(string[] list)
+{
+    var dictionary = makeCounterDictionary(list);
+    foreach (var item in list)
+    {
+        if (dictionary[item] > 1)
+        {
+            return item;
+        }
+    }
+    return null;
+}
+
+void main()
+{
+    var list = new string[] { "a", "b", "c", "d", "c", "e", "f" };
+    Console.WriteLine("Первое повторяющееся значение = " + getFirstDuplicatedElem(list));
+}
+main();
