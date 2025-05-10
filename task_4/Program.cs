00f402c80493c18489b6a2ae2bbd0b5576321b15
Veronica <no.demchenko@yandex.ru>
2025-05-10 21:54:25 +0300

    feat: задача 4

---
+++ task_4/Program.cs
@@ -0,0 +1,44 @@
+﻿Dictionary<char, int> makeCounterDictionary(char[] charArray)
+{
+    Dictionary<char, int> map = new Dictionary<char, int>();
+    foreach (var item in charArray)
+    {
+        if (!Char.IsLetter(item))
+        {
+            continue;
+        }
+        if (map.ContainsKey(item))
+        {
+            map[item] = map[item] + 1;
+        }
+        else
+        {
+            map.Add(item, 1);
+        }
+    }
+    return map;
+}
+
+// возвращает первую неповторяющуюся букву в слове
+Nullable<char> getLetterNotDuplicatedAt(string word)
+{
+    var arrayFromWord = word.ToCharArray();
+
+    var dictionary = makeCounterDictionary(arrayFromWord);
+    foreach (var item in arrayFromWord)
+    {
+        if (dictionary[item] == 1)
+        {
+            return item;
+        }
+    }
+    return null;
+}
+
+void main()
+{
+    string word = "minimum";
+    Console.WriteLine(getLetterNotDuplicatedAt(word));
+}
+
+main();
