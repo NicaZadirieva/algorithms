b96d6473bd4f6eafaa0ad493874be794f3edb40d
Veronica <no.demchenko@yandex.ru>
2025-05-11 18:11:08 +0300

    feat(task_4): первая реализация

---
+++ task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// возвращает индекс первого 'x' в строке
+int getIndexOfFirstX(string word, int foundAtIndex = 0)
+{
+    if (word[foundAtIndex] == 'x')
+    {
+        return foundAtIndex;
+    }
+    else
+    {
+        return getIndexOfFirstX(word, foundAtIndex + 1);
+    }
+}
+
+void main()
+{
+    string word = "abcdefghijklmnopqrstuvwxyz";
+    Console.WriteLine(getIndexOfFirstX(word));
+}
+
+main();
