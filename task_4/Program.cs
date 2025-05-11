43254a0dbb5878a75f0c999a2aa539fc462c1034
Veronica <no.demchenko@yandex.ru>
2025-05-11 14:02:00 +0300

    feat: реализация обращения строки через стек

---
+++ task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// обращение строки через стек
+string reserved(string word)
+{
+    string reserved = "";
+    Stack<char> lettersStack = new Stack<char>(word.ToCharArray());
+    foreach (var item in lettersStack)
+    {
+        reserved += item.ToString();
+    }
+    return reserved;
+}
+
+void main()
+{
+    string word = "abcde";
+    Console.WriteLine(reserved(word));
+}
+
+main();
