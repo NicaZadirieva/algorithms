587d25430dfd3729a2e397ec632939e2c9bcb592
Veronica <no.demchenko@yandex.ru>
2025-05-11 17:38:16 +0300

    feat: 1 задача

---
+++ task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// возвращает количество букв во всех словах в массиве
+int getSummaryCount(string[] words)
+{
+    if (words.Length == 0)
+    {
+        return 0;
+    }
+    return words.Length + getSummaryCount(words.AsSpan(1, words.Length - 1).ToArray());
+}
+
+void main()
+{
+    string[] words = { "ab", "c", "def", "ghij" };
+    Console.WriteLine(getSummaryCount(words));
+}
+
+main();
