4079cb6bc8d22b8d1747710325196e7d90295b10
Veronica <no.demchenko@yandex.ru>
2025-05-11 17:48:05 +0300

    feat(task_2): первая реализация

---
+++ task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿List<int> getOnlyEvenNumbers(int index, List<int> numbers, List<int> result)
+{
+    if (numbers.Count == index)
+    {
+        return result;
+    }
+    if (numbers[index] % 2 == 0)
+    {
+        result.Add(numbers[index]);
+    }
+    return getOnlyEvenNumbers(index + 1, numbers, result);
+}
+
+void main()
+{
+    List<int> numbers = new List<int>([1, 2, 4, 5, 6, 10]);
+    var evenNumbers = getOnlyEvenNumbers(0, numbers, new List<int>());
+    foreach (var item in evenNumbers)
+    {
+        Console.WriteLine(item);
+        Console.WriteLine(" ");
+    }
+}
+
+main();
