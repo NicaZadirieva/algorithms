2b1317670945edf550e9d89703328d8e3b218c15
Veronica <no.demchenko@yandex.ru>
2025-05-11 18:02:21 +0300

    feat: вычисление треугольного числа

---
+++ task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// вычисление треугольных чисел
+int getNTriangularNumber(int N)
+{
+    if (N == 1)
+    {
+        return 1;
+    }
+    return N + getNTriangularNumber(N - 1);
+}
+
+void main()
+{
+    Console.WriteLine(getNTriangularNumber(7));
+}
+
+main();
