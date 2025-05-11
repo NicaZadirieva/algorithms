c3ab52eeb1a716c6ea86acad49c3930f362b5552
Veronica <no.demchenko@yandex.ru>
2025-05-11 18:33:35 +0300

    feat: первая реализация поиска кратчайшего пути

---
+++ task_5/Program.cs
@@ -0,0 +1,18 @@
+﻿int calcShortestPaths(int X, int Y, int tempX = 0, int tempY = 0)
+{
+    if (tempX == X && tempY == Y)
+    {
+        return 1;
+    }
+    if (tempX > X || tempY > Y)
+    {
+        return 0;
+    }
+    return calcShortestPaths(X, Y, tempX + 1, tempY) + calcShortestPaths(X, Y, tempX, tempY + 1);
+}
+
+void main()
+{
+    Console.WriteLine(calcShortestPaths(3, 7));
+}
+main();
