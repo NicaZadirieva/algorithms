fadd99b05cb9215e2fa9314379d5a411ef74736a
Veronica <no.demchenko@yandex.ru>
2025-05-14 09:13:08 +0300

    feat: произведение 3 самых больших за NlogN

---
+++ task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int getLargestProduct(int[] numbers)
+{
+    if (numbers.Length > 2)
+    {
+        int[] numbersCopy = new int[numbers.Length];
+        System.Array.Copy(numbers, numbersCopy, numbers.Length);
+        System.Array.Sort(numbersCopy);
+        return numbersCopy[numbersCopy.Length - 1]
+            * numbersCopy[numbersCopy.Length - 2]
+            * numbersCopy[numbersCopy.Length - 3];
+    }
+    return 0;
+}
+int[] numbers = new int[] { 1, 2, 3, 4, 5, 1 };
+Console.WriteLine(getLargestProduct(numbers));
