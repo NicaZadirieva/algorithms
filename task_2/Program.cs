624e0f535298c1594b1013ffbcc7cef9501dbdab
Veronica <no.demchenko@yandex.ru>
2025-05-14 09:19:29 +0300

    feat: реализация для улучшения

---
+++ task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Nullable<int> findMissingNumber(int[] array)
+{
+    for (var i = 0; i < array.Length; i++)
+    {
+        if (Array.IndexOf(array, array[i]) >= 0)
+        {
+            return i;
+        }
+    }
+    return null;
+}
+
+Console.WriteLine(findMissingNumber(new int[] { 1, 2, 3 }));
