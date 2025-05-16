4caa46bb43eb77a090d4d0b69a8a66305ab60503
NicaZadirieva <no.demchenko@yandex.ru>
2025-05-16 12:15:40 +0300

    feat: reverse linkedlist
    O(n)

---
+++ task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿LinkedList<int> ints = new LinkedList<int>([1, 2, 3, 4, 5]);
+
+void ReverseInPlace<T>(LinkedList<T> list)
+{
+    if (list.Count <= 1)
+        return;
+
+    LinkedListNode<T> current = list.First;
+    while (current != null)
+    {
+        LinkedListNode<T> next = current.Next;
+        list.Remove(current);
+        list.AddFirst(current); // Перемещаем текущий узел в начало
+        current = next;
+    }
+}
+ReverseInPlace(ints);
+
+foreach (int i in ints)
+{
+    Console.WriteLine(i);
+}
