d8c24c250f7c6e2bd9c7796a12eaae7505334b97
NicaZadirieva <no.demchenko@yandex.ru>
2025-05-16 11:32:18 +0300

    demo: печать с конца

---
+++ task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿LinkedList<int> ints = new LinkedList<int>([1, 2, 3, 4, 5]);
+
+// с конца до начала списка
+var currentNode = ints.Last;
+while (currentNode != null)
+{
+    Console.WriteLine(currentNode.Value);
+    currentNode = currentNode.Previous;
+}
